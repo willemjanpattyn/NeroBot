8a3a32c50f0d56c476b2d6847e49fecc9ff9914e
PattynW <willemjan.pattyn@gmail.com>
2017-11-12 22:29:36 +0100

    Fix path

--- NeroBot/run.js
+++ NeroBot/run.js
@@ -13,7 +13,7 @@ client.registry
     // Registers all built-in groups, commands, and argument types
     .registerDefaults()
     // Registers all of your commands in the ./commands/ directory
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+    .registerCommandsIn(path.join(__dirname, '/commands'));
 
 
 //client.on('read', () => {
