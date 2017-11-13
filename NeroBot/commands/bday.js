cfbfe13975c946e5155f8dfc51e7e7fa9ef9c6b9
PattynW <willemjan.pattyn@gmail.com>
2017-11-13 14:42:43 +0100

    changed connection again

--- NeroBot/commands/bday.js
+++ NeroBot/commands/bday.js
@@ -1,17 +1,17 @@
-const { Client } = require("pg");
-
-const db = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-});
-
-db.connect();
 exports.run = (client, message, args) => {
+    const { Client } = require("pg");
 
+    const db = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    });
+
+    db.connect();
     db.query("SELECT * FROM bdays;", (err, result) => {
         if (err) throw err;
         console.log(JSON.stringify(res));
         message.channel.send(JSON.stringify(res));
     });
+    db.end();
 }
-db.end();
+
