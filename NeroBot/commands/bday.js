210c0174355a3daf08ff8ff0470ed721878a4b50
PattynW <willemjan.pattyn@gmail.com>
2017-11-13 17:06:24 +0100

    renamed column birthday

--- NeroBot/commands/bday.js
+++ NeroBot/commands/bday.js
@@ -8,13 +8,13 @@ exports.run = (client, message, args) => {
         console.log(err);
     }
     console.log("Sucessfully conencted to DB");
-    db.query("SELECT * FROM bdays ORDER BY birthday2;", (err, result) => {
+    db.query("SELECT * FROM bdays ORDER BY birthday;", (err, result) => {
         if (err) throw err;
         var output = "Here is the list of birthdays, Praetor!\n```";
         for (let row of result.rows) {
             console.log(JSON.stringify(row));
 
-            var formattedDate = "" + row.birthday2;
+            var formattedDate = "" + row.birthday;
             console.log(formattedDate);
             var month = formattedDate.substring(4, 7);
             var day = formattedDate.substring(8, 10);
