8dfbf9fd9ac13969938ecc406e7a9fb19fca77fe
PattynW <willemjan.pattyn@gmail.com>
2017-11-14 19:01:50 +0100

    changed if else structure

--- NeroBot/commands/command.js
+++ NeroBot/commands/command.js
@@ -16,21 +16,23 @@ exports.run = (client, message, args) => {
 	if (args.length != 2 || !args[0].startsWith(prefix)) {
 		message.channel.send("Please input the correct command format\n```!command !yourCommand http://i.imgur.com/YrgluxT.gif ```");
 	}
-	else if (!args[1].startsWith("http") && args[1].match(/\.(jpeg|jpg|gif|png)$/) != null) {
-		console.log(args[1].startsWith("http"));
-		console.log(args[1].match(/\.(jpeg|jpg|gif|png)$/));
-		message.channel.send("Please input a correct image URL (.png, .jpg, gif)");
-	}
 	else {
-		db.query(`INSERT INTO commands VALUES ('${args[0]}','${args[1]}');`, (err, result) => {
-			if (err) {
-				message.channel.send("Command may already exist, please use a different name!");
-				console.log(err);
-				return;
-			}
-			var output = "\n```";
-			output += args[0] + " " + args[1] + "```";
-			message.channel.send("New command has been created" + output);
-		});
+		if (!args[1].startsWith("http") || args[1].match(/\.(jpeg|jpg|gif|png)$/) == null) {
+			console.log(args[1].startsWith("http"));
+			console.log(args[1].match(/\.(jpeg|jpg|gif|png)$/));
+			message.channel.send("Please input a correct image URL (.png, .jpg, gif)");
+		}
+		else {
+			db.query(`INSERT INTO commands VALUES ('${args[0]}','${args[1]}');`, (err, result) => {
+				if (err) {
+					message.channel.send("Command may already exist, please use a different name!");
+					console.log(err);
+					return;
+				}
+				var output = "\n```";
+				output += args[0] + " " + args[1] + "```";
+				message.channel.send("New command has been created" + output);
+			});
+		}
 	}
 }
