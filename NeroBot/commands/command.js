84006224c0a6f1240f26967d4fb985961ca2c97c
PattynW <willemjan.pattyn@gmail.com>
2017-11-14 18:38:32 +0100

    used regex for image URL

--- NeroBot/commands/command.js
+++ NeroBot/commands/command.js
@@ -2,6 +2,12 @@ exports.run = (client, message, args) => {
 	var runNode = require("../run.js");
 	var db = runNode.db;
 	var prefix = runNode.prefix;
+	var pattern = new RegExp('^(https?:\/\/)?' + // protocol
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|' + // domain name
+    '((\d{1,3}\.){3}\d{1,3}))' + // OR ip (v4) address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*' + // port and path
+    '(\?[;&a-z\d%_.~+=-]*)?' + // query string
+    '(\#[-a-z\d_]*)?$', 'i'); // fragment locater
 
 	try {
 		if (db == null)
@@ -12,15 +18,13 @@ exports.run = (client, message, args) => {
 	}
 
 	console.log(args[0] + " " + args[1]);
+
 	if (args.length != 2 || !args[0].startsWith(prefix)) {
 		message.channel.send("Please input the correct command format\n```!command !yourCommand http://i.imgur.com/YrgluxT.gif ```");
 	}
-	else if (!args[1].startsWith("http") || !args[1].endsWith(".png") || !args[1].endsWith(".jpg") || !args[1].endsWith(".gif")) {
-		console.log(args[1].startsWith("http"));
-		console.log(args[1].endsWith(".png"));
-		console.log(args[1].endsWith(".jpg"));
-		console.log(args[1].endsWith(".gif"));
-
+	else if (!pattern.test(args[1]) && !args[1].match(/\.(jpeg|jpg|gif|png)$/)) {
+		console.log(pattern.test(args[1]));
+		console.log(args[1].match(/\.(jpeg|jpg|gif|png)$/));
 		message.channel.send("Please input a correct image URL (.png, .jpg, gif)");
 	}
 	else {
