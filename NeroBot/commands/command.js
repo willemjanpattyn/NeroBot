bd714cb8aad377e7b3e4ae44a9a5b93b9670080e
PattynW <willemjan.pattyn@gmail.com>
2017-11-14 20:59:52 +0100

    added list command

--- NeroBot/commands/command.js
+++ NeroBot/commands/command.js
@@ -5,26 +5,44 @@ exports.run = (client, message, args) => {
 
 	console.log(args[0] + " " + args[1]);
 
-	if (args.length != 2 || !args[0].startsWith(prefix)) {
-		message.channel.send("Please input the correct command format\n```!command !yourCommand http://i.imgur.com/YrgluxT.gif ```");
-	}
-	else {
-		if (!args[1].startsWith("http") || args[1].match(/\.(jpeg|jpg|gif|png)$/) == null) {
-			console.log(args[1].startsWith("http"));
-			console.log(args[1].match(/\.(jpeg|jpg|gif|png)$/));
-			message.channel.send("Please input a correct image URL (.png, .jpg, gif)");
-		}
-		else {
-			db.query(`INSERT INTO commands VALUES ('${args[0]}','${args[1]}');`, (err, result) => {
-				if (err) {
-					message.channel.send("Command may already exist, please use a different name!");
-					console.log(err);
-					return;
-				}
-				var output = "\n```";
-				output += args[0] + " " + args[1] + "```";
-				message.channel.send("New command has been created" + output);
-			});
-		}
-	}
+    if (args[0] == "list") {
+        db.query("SELECT * FROM commands;", (err, result) => {
+            if (err) return console.log(err);
+
+            let output = "```";
+
+            for (let row of result.rows) {
+                var index = 1;
+                output += index + ". " + row.command_name + "\n";
+                index++;
+            }
+            output += "```";
+
+            message.channel.send("List of available custom commands\n" + output);
+        });
+    }
+    else {
+        if (args.length != 2 || !args[0].startsWith(prefix)) {
+            message.channel.send("Please input the correct command format\n```!command !yourCommand (http://i.imgur.com/YrgluxT.gif)```");
+        }
+        else {
+            if (!args[1].startsWith("http") || args[1].match(/\.(jpeg|jpg|gif|png)$/) == null) {
+                console.log(args[1].startsWith("http"));
+                console.log(args[1].match(/\.(jpeg|jpg|gif|png)$/));
+                message.channel.send("Please input a correct image URL (.png, .jpg, gif)");
+            }
+            else {
+                db.query(`INSERT INTO commands VALUES ('${args[0]}','${args[1]}');`, (err, result) => {
+                    if (err) {
+                        message.channel.send("Command may already exist, please use a different name!");
+                        console.log(err);
+                        return;
+                    }
+                    var output = "\n```";
+                    output += args[0] + " " + args[1] + "```";
+                    message.channel.send("New command has been created" + output);
+                });
+            }
+        }
+    }
 }
