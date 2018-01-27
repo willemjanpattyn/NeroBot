e108158661dad099b48d93ef4274c813cb27a5eb
Willem-Jan Pattyn <willemjan.pattyn@gmail.com>
2018-01-27 16:03:25 +0100

    Fixed bug in countdown not properly ending

--- NeroBot/run.js
+++ NeroBot/run.js
@@ -43,7 +43,7 @@ function getDaysUntil() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    if (daysLeft <= 0) {
+    if (days <= 0) {
         client.user.setGame("Fate/EXTRA ANIME IS OUT!!!");
     }
     else {
