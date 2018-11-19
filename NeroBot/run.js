050e82f885ca9ae59d263e8971190bb39ad26726
Willem-Jan Pattyn <willemjan.pattyn@gmail.com>
2018-11-19 13:58:50 +0100

    Update run.js

--- NeroBot/run.js
+++ NeroBot/run.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 const { Client } = require("pg");
 
 const gwId = "434375055917711360";
