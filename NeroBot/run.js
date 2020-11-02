8dfc137a5804d2af4bda2869cf9babe0ba0776ed
Willem-Jan Pattyn <willem-jan.pattyn@student.ehb.be>
2020-11-02 14:07:10 +0100

    use channel cache to find channel by name

--- NeroBot/run.js
+++ NeroBot/run.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -429,9 +431,10 @@ client.on("message", async (message) => {
 client.on("guildMemberAdd", (member) => {
   // const roleID = "466718453836021770";
   // member.addRole(roleID).then(console.log).catch(console.error);
-  const channel = member.guild.channels.find("name", "general");
-  let guildIcon = member.guild.iconURL;
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
   if (!channel) return;
+  let guildIcon = member.guild.iconURL;
+  
   channel.send({
     embed: {
       color: 0xbf0000,
