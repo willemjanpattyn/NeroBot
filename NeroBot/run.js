2091b371ce814189a9bd0ebf694f9217dc9b27f8
Willem-Jan Pattyn <willem-jan.pattyn@student.ehb.be>
2020-11-02 14:14:06 +0100

    code cleanup

--- NeroBot/run.js
+++ NeroBot/run.js
@@ -36,120 +36,142 @@ client.on("ready", () => {
   exports.db = db;
 });
 
+client.on("guildMemberAdd", member => {
+  // const roleID = "466718453836021770";
+  // member.addRole(roleID).then(console.log).catch(console.error);
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+  if (!channel) return;
+  let guildIcon = member.guild.iconURL;
+  
+  channel.send({
+    embed: {
+      color: 0xbf0000,
+      title: "Welcome to the Nero Mancave!",
+      description: `${member}, for rules and more information on the server, please check <#549612774431391747>. If you wish to change your color and get a role name, you can mention them in <#348786731421794315>!
+                \nEnjoy your stay! <:umu:473851038592663552>`,
+      thumbnail: { url: guildIcon },
+      image: { url: "https://i.imgur.com/CUS8GGh.gif" },
+    },
+  });
+  console.log(
+    `USERJOIN_LOG: User ${member.username} (${member.id}) joined the server.`
+  );
+});
+
 process.on("unhandledRejection", (error) => {
   console.log("unhandledRejection", error.message);
 });
 
 //Grants regular role to regular users
-function grantRegularRole() {
-  const roleID = "513678231656988692";
-  const guildID = "343061617275174912";
-  const apiKey = process.env.API_KEY;
-  const apiHost = "https://api.tatsumaki.xyz";
+// function grantRegularRole() {
+//   const roleID = "513678231656988692";
+//   const guildID = "343061617275174912";
+//   const apiKey = process.env.API_KEY;
+//   const apiHost = "https://api.tatsumaki.xyz";
 
-  let members = client.guilds.first().members;
+//   let members = client.guilds.first().members;
 
-  let xhttp = new XMLHttpRequest();
+//   let xhttp = new XMLHttpRequest();
 
-  xhttp.open("GET", apiHost + "/guilds/" + guildID + "/leaderboard", true);
-  xhttp.setRequestHeader("Content-Type", "application/json");
-  xhttp.setRequestHeader("Authorization", apiKey);
-  console.log(xhttp.status);
-  xhttp.onload = function () {
-    if (xhttp.status >= 200 && xhttp.status < 400) {
-      let data = JSON.parse(this.responseText);
-      data.forEach((guildMember) => {
-        if (
-          guildMember != null &&
-          members.find("id", guildMember.user_id) != null
-        ) {
-          if (
-            guildMember.score >= 10000 &&
-            !members.find("id", guildMember.user_id).roles.has(roleID)
-          ) {
-            members.find("id", guildMember.user_id).addRole(roleID);
-          }
-        }
-      });
-    } else {
-      console.log("error");
-    }
-  };
-  xhttp.send();
-}
+//   xhttp.open("GET", apiHost + "/guilds/" + guildID + "/leaderboard", true);
+//   xhttp.setRequestHeader("Content-Type", "application/json");
+//   xhttp.setRequestHeader("Authorization", apiKey);
+//   console.log(xhttp.status);
+//   xhttp.onload = function () {
+//     if (xhttp.status >= 200 && xhttp.status < 400) {
+//       let data = JSON.parse(this.responseText);
+//       data.forEach((guildMember) => {
+//         if (
+//           guildMember != null &&
+//           members.find("id", guildMember.user_id) != null
+//         ) {
+//           if (
+//             guildMember.score >= 10000 &&
+//             !members.find("id", guildMember.user_id).roles.has(roleID)
+//           ) {
+//             members.find("id", guildMember.user_id).addRole(roleID);
+//           }
+//         }
+//       });
+//     } else {
+//       console.log("error");
+//     }
+//   };
+//   xhttp.send();
+// }
+
+// client.on("message", async (message) => {
+//   if (
+//     message.content == "@!grantRegular" &&
+//     message.author.id == "232430593193803777"
+//   ) {
+//     grantRegularRole();
+//   }
+// });
+
+// var episodeCount = 7;
+// function getDaysUntil() {
+//   db.query(
+//     `SELECT * FROM fate_extra_eps WHERE episode_count = ${episodeCount};`,
+//     (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         for (let row of result.rows) {
+//           //var releaseDate = new Date("Jan 27, 2018 16:00:00");
+//           var releaseDate = new Date(row.release_date);
+//           releaseDate.setHours(15, 0, 0, 0);
+
+//           var oneDay = 24 * 60 * 60 * 1000;
+//           var today = new Date();
+//           var daysLeft = (releaseDate.getTime() - today.getTime()) / oneDay;
+//           console.log(daysLeft);
+//           var distance = releaseDate.getTime() - today.getTime();
+
+//           var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//           var hours = Math.floor(
+//             (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+//           );
+//           var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//           var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//           if (daysLeft <= 0) {
+//             //client.user.setGame("Fate/EXTRA IS OUT!!!");
+//             episodeCount++;
+//             console.log("Added +1 to episodeCount");
+//           } else {
+//             //client.user.setGame("Episode " + episodeCount + days + "d " + hours + "h " + minutes + "m " + "left until Fate/EXTRA!!");
+//             client.user.setGame(
+//               `${days}d ${hours}h ${minutes}m left until Fate/EXTRA ep ${episodeCount}!!`
+//             );
+//             //console.log(`${days}d ${hours}h ${minutes}m left until Fate/EXTRA ep ${episodeCount}!!`);
+//           }
+//         }
+//       }
+//     }
+//   );
+// }
 
 client.on("message", async (message) => {
-  if (
-    message.content == "@!grantRegular" &&
-    message.author.id == "232430593193803777"
-  ) {
-    grantRegularRole();
-  }
-});
+  // if (
+  //   message.content.includes("Paused") &&
+  //   message.channel.name == "music" &&
+  //   message.author.id == "381425973356134400"
+  // ) {
+  //   message.channel.send({
+  //     files: ["https://i.imgur.com/61qtRJu.png"],
+  //   });
+  // }
 
-var episodeCount = 7;
-function getDaysUntil() {
-  db.query(
-    `SELECT * FROM fate_extra_eps WHERE episode_count = ${episodeCount};`,
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        for (let row of result.rows) {
-          //var releaseDate = new Date("Jan 27, 2018 16:00:00");
-          var releaseDate = new Date(row.release_date);
-          releaseDate.setHours(15, 0, 0, 0);
-
-          var oneDay = 24 * 60 * 60 * 1000;
-          var today = new Date();
-          var daysLeft = (releaseDate.getTime() - today.getTime()) / oneDay;
-          console.log(daysLeft);
-          var distance = releaseDate.getTime() - today.getTime();
-
-          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-          var hours = Math.floor(
-            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-          );
-          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-          if (daysLeft <= 0) {
-            //client.user.setGame("Fate/EXTRA IS OUT!!!");
-            episodeCount++;
-            console.log("Added +1 to episodeCount");
-          } else {
-            //client.user.setGame("Episode " + episodeCount + days + "d " + hours + "h " + minutes + "m " + "left until Fate/EXTRA!!");
-            client.user.setGame(
-              `${days}d ${hours}h ${minutes}m left until Fate/EXTRA ep ${episodeCount}!!`
-            );
-            //console.log(`${days}d ${hours}h ${minutes}m left until Fate/EXTRA ep ${episodeCount}!!`);
-          }
-        }
-      }
-    }
-  );
-}
-
-client.on("message", async (message) => {
-  if (
-    message.content.includes("Paused") &&
-    message.channel.name == "music" &&
-    message.author.id == "381425973356134400"
-  ) {
-    message.channel.send({
-      files: ["https://i.imgur.com/61qtRJu.png"],
-    });
-  }
-
-  if (
-    message.content.includes("The player has stopped") &&
-    message.channel.name == "music" &&
-    message.author.id == "381425973356134400"
-  ) {
-    message.channel.send({
-      files: ["https://i.imgur.com/tJIDqYs.png"],
-    });
-  }
+  // if (
+  //   message.content.includes("The player has stopped") &&
+  //   message.channel.name == "music" &&
+  //   message.author.id == "381425973356134400"
+  // ) {
+  //   message.channel.send({
+  //     files: ["https://i.imgur.com/tJIDqYs.png"],
+  //   });
+  // }
 
   if (message.author.bot) return;
 
@@ -426,28 +448,6 @@ client.on("message", async (message) => {
     );
     //console.log(`COMMAND_LOG: User ${message.author.username} (${message.author.id}) issued !command command`);
   }
-});
-
-client.on("guildMemberAdd", (member) => {
-  // const roleID = "466718453836021770";
-  // member.addRole(roleID).then(console.log).catch(console.error);
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
-  if (!channel) return;
-  let guildIcon = member.guild.iconURL;
-  
-  channel.send({
-    embed: {
-      color: 0xbf0000,
-      title: "Welcome to the Nero Mancave!",
-      description: `${member}, for rules and more information on the server, please check <#549612774431391747>. If you wish to change your color and get a role name, you can mention them in <#348786731421794315>!
-                \nEnjoy your stay! <:umu:473851038592663552>`,
-      thumbnail: { url: guildIcon },
-      image: { url: "https://i.imgur.com/CUS8GGh.gif" },
-    },
-  });
-  console.log(
-    `USERJOIN_LOG: User ${member.username} (${member.id}) joined the server.`
-  );
 });
 
 //Login
