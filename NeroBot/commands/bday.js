f34e0a9af09f5017fe7a83f8f654f92543062d88
Willem-Jan Pattyn <willemjan.pattyn@gmail.com>
2023-01-15 22:54:03 +0100

    legacy bday + help fix

--- NeroBot/commands/bday.js
+++ NeroBot/commands/bday.js
@@ -99,6 +99,7 @@ function getBday(user) {
                    var output = `**${user.username}** hasn't set their birthday yet.`;
                 }
                 else {
+                    console.log(user);
                     for (let row of result.rows) {
                         let formattedDate = "" + row.birthday;
                         let month = formattedDate.substring(4, 7);
@@ -177,126 +178,41 @@ module.exports = {
         }
 		await interaction.editReply(reply);
 	},
-};
-
-exports.run = (client, message, args) => {
-    const db = require("../run.js").db;
-    //Show list of birthdays
-    if (args == "") {
-        if (message.channel.name != "my-room" && message.channel.name != "bot-testing") return; // Ignore all channels except #my-room
-        message.channel.send("test");
-        // const padEnd = require("pad-end");
-
-        // db.query("SELECT * FROM bdays ORDER BY birthday;", (err, result) => {
-        //     if (err) {
-        //         message.channel.send("Something went wrong...");
-        //         console.log(err);
-        //     }
-        //     else {
-        //         var output = ":cake: | **Nero Mancave Birthday List**\n```";
-        //         for (let row of result.rows) {
-        //             if (message.guild.members.cache.get(row.user_id) != null) {
-        //                 let u = message.guild.members.cache.get(row.user_id).displayName;
-        //                 let formattedDate = "" + row.birthday;
-        //                 let month = formattedDate.substring(4, 7);
-        //                 let day = formattedDate.substring(8, 10);
-        //                 output += month + " " + padEnd(day, 15, "") + u + "\n";
-        //                 //output += padEnd(u, 50, "") + "\t" + month + " " + day + "\n";
-        //             }
-        //         }
-        //         output += "```";
-        //         message.channel.send(output);
-        //     }
-        // });
-    }
-    //Set birthday
-    else if (args[0].toLowerCase() == "set") {
-        let givenBday = "" + args[1];
-        let u = message.author;
-        var day = givenBday.split('/')[0];
-        var month = givenBday.substring(givenBday.lastIndexOf('/') + 1);
-        console.log(day + " " + month);
-
-        db.query(`SELECT * FROM bdays WHERE user_id = '${u.id}';`, (err, result) => {
-            if (err) {
-                message.channel.send("Something went wrong...");
-                console.log(err);
-            }
-            console.log(result.rowCount);
-            if (result.rowCount > 0) {
-                db.query(`UPDATE bdays SET birthday = '2000-${month}-${day}' WHERE user_id = '${u.id}';`, (err, result) => {
-                    if (err) {
-                        message.channel.send("Please input a correct date format [DD/MM]...");
-                        console.log(err);
-                    }
-                    else {
-                        console.log(`COMMAND_LOG: ${message.author.username} (${message.author.id}) updated their birthday to ${givenBday}`);
-                        message.channel.send(`Your birthday has been updated to **${givenBday}**!`);
-                    }
-                });
-            }
-            else {
-                db.query(`INSERT INTO bdays (user_id, username, birthday) VALUES ('${u.id}', '${u.username}', '2000-${month}-${day}');`, (err, result) => {
-                    if (err) {
-                        message.channel.send("Please input a correct date format [DD/MM]...");
-                        console.log(err);
-                    }
-                    else {
-                        console.log(`COMMAND_LOG: ${message.author.username} (${message.author.id}) set their birthday to ${givenBday}`);
-                        message.channel.send(`Your birthday has been set to **${givenBday}**!`);
-                    }
-                });
-            }
-        });
-    }
-    //Show user birthday
-    else {
-        var u = null;
-        if (message.mentions.members.first()) {
-            u = message.mentions.members.first();
+    run: async (client, message, args) => {
+        const db = require("../run.js").db;
+        //Show list of birthdays
+        if (args == "") {
+            if (message.channel.name != "my-room" && message.channel.name != "bot-testing") return; // Ignore all channels except #my-room
+            let output = await getBdayList(message);
+            message.channel.send(output);
         }
+        //Set birthday
+        else if (args[0].toLowerCase() == "set") {
+            let givenBday = "" + args[1];
+            let u = message.author;
+            var day = givenBday.split('/')[0];
+            var month = givenBday.substring(givenBday.lastIndexOf('/') + 1);
+            console.log(day + " " + month);
+            
+            let output = await setBday(u,day,month);
+            message.channel.send(output);
+        }
+        //Show user birthday
         else {
-            var query = args.join(' ').toLowerCase();
-            u = message.guild.members.cache.filter(u => u.user.username.toLowerCase().includes(query) || u.displayName.toLowerCase().includes(query)).first();
-        }
-        if (u == null) {
-            return message.channel.send("No user found.");
-        }
-
-        db.query(`SELECT * FROM bdays WHERE user_id = '${u.user.id}';`, (err, result) => {
-            if (err) {
-                console.log(err);
-                return message.channel.send("Please input a correct user...");
-            }
-            if (result.rows <= 0) {
-                return message.channel.send(`**${u.user.username}** hasn't set their birthday yet.`);
+            var u = null;
+            if (message.mentions.members.first()) {
+                u = message.mentions.members.first().user;
             }
             else {
-                for (let row of result.rows) {
-                    let formattedDate = "" + row.birthday;
-                    let month = formattedDate.substring(4, 7);
-                    let day = formattedDate.substring(8, 10);
-
-                    var dateResult = row.birthday;
-                    //console.log(dateResult);
-                    var currentDate = new Date();
-                    currentDate.setFullYear(2000);
-                    //console.log(currentDate);
-
-                    if (currentDate > dateResult) {
-                        dateResult.setFullYear(2001);
-                    }
-
-                    var amountOfDays = Math.floor(Math.abs((currentDate.getTime() - dateResult.getTime()) / (24 * 60 * 60 * 1000))) + 1;
-
-                    if (amountOfDays == 0 || amountOfDays == 365) {
-                        message.channel.send(`**${u.user.username}**'s birthday is on ${month} ${day}, which is today! Happy Birthday, ${u}!!\nhttps://www.youtube.com/watch?v=IylJ_daGouw`);
-                    }
-                    else {
-                        message.channel.send(`**${u.user.username}**'s birthday is on ${month} ${day}! (${amountOfDays} day(s) remaining)`);
-                    }
-                }
+                var query = args.join(' ').toLowerCase();
+                u = message.guild.members.cache.filter(u => u.user.username.toLowerCase().includes(query) || u.displayName.toLowerCase().includes(query)).first().user;
             }
-        });
+            if (u == null) {
+                return message.channel.send("No user found.");
+            }
+            //console.log(u);
+            let output = await getBday(u);
+            message.channel.send(output);
+        }
     }
-}
+};
--- NeroBot/commands/help.js
+++ NeroBot/commands/help.js
@@ -1,5 +1,45 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+function getHelpText(category) {
+    switch(category) {
+        case 'bday': 
+            output = "`!bday` __`View birthdays of members or set your own birthday`__\n\n";
+            output += "**Usage:** `!bday [set] <DD/MM>` | `!bday <user>`\n\n";
+            output += "**Examples:**\n`!bday` Shows list of birthdays of members\n`!bday set 12/05` Set or update your birthday\n`!bday Nero` View user's birthday (search or mention)\n\n";
+            break;
+        case 'fgo':
+            output = "`!fgo-show | !fgo-edit` __`View or update FGO profile`__\n\n";
+            output += "**Usage:** `!fgo-show <user>` | `!fgo-edit [IGN: ign | ID: id | Region: <JP|NA|TW|CH|KR>]` [Add screenshot as attachment!]\n\n";
+            output += "**Examples:**\n`!fgo-show` Shows own FGO profile\n";
+            output += "`!fgo-show Nero` Shows user's FGO profile (search or mention)\n";
+            output += "`!fgo-edit IGN: Duwangel | ID: 123,456,789 | Region: JP` Creates or updates your FGO profile\n";
+            output += "`!fgo-edit IGN: Yosoku Assistant ` Updates only your IGN";
+            break;
+        case 'opt':
+            output = "`!optin | !optout` __`Adds or removes mentionable role`__\n\n";
+            output += "**Usage:** `!optin <role>` | `!optout <role>`\n";
+            output += "**Args:** `gw`\n\n";
+            output += "**Examples:**\n`!optin gw` Opts you in the Group Watch role\n";
+            output += "`!optout gw` Opts you out of the Group Watch role";
+            break;
+        case 'command':
+            output = "__`View custom command list or set your own commands`__\n\n";        
+            output += "**Usage examples:**\n`!cl` Shows list of of available custom created commands as a DM (Due to character limit per post, Nero will send several messages) [DEPRECATED]\n";
+            output += "`!add !yourcommand http://i.imgur.com/YrgluxT.gif` Adds your own command\n";
+            output += "`!find search_value` Find a command\n";
+            output += "`!rename !old !new` Rename a command\n";
+            output += "`!edit !yourcommand http://i.imgur.com/YrgluxT.gif` Edits value of the command\n";
+            output += "`!delete !yourcommand` Deletes the command [Admin only]\n";
+            break;
+        default:
+            output = ":clipboard: | **Nero Command List**\n";
+            output += "Use `!help <command>` to get more info on a specific command, for example: `!help bday`\n\n";
+            output += "`bday`, `command`, `opt`, `fgo`";
+    }
+
+    return output;
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
@@ -17,92 +57,19 @@ module.exports = {
             ),
 	async execute(interaction) {
         const category = interaction.options.getString('category');
-        let output = ""
-        switch(category) {
-            case 'bday': 
-                output = "`!bday` __`View birthdays of members or set your own birthday`__\n\n";
-                output += "**Usage:** `!bday [set] <DD/MM>` | `!bday <user>`\n\n";
-                output += "**Examples:**\n`!bday` Shows list of birthdays of members\n`!bday set 12/05` Set or update your birthday\n`!bday Nero` View user's birthday (search or mention)\n\n";
-                break;
-            case 'fgo':
-                output = "`!fgo-show | !fgo-edit` __`View or update FGO profile`__\n\n";
-                output += "**Usage:** `!fgo-show <user>` | `!fgo-edit [IGN: ign | ID: id | Region: <JP|NA|TW|CH|KR>]` [Add screenshot as attachment!]\n\n";
-                output += "**Examples:**\n`!fgo-show` Shows own FGO profile\n";
-                output += "`!fgo-show Nero` Shows user's FGO profile (search or mention)\n";
-                output += "`!fgo-edit IGN: Duwangel | ID: 123,456,789 | Region: JP` Creates or updates your FGO profile\n";
-                output += "`!fgo-edit IGN: Yosoku Assistant ` Updates only your IGN";
-                break;
-            case 'opt':
-                output = "`!optin | !optout` __`Adds or removes mentionable role`__\n\n";
-                output += "**Usage:** `!optin <role>` | `!optout <role>`\n";
-                output += "**Args:** `gw`\n\n";
-                output += "**Examples:**\n`!optin gw` Opts you in the Group Watch role\n";
-                output += "`!optout gw` Opts you out of the Group Watch role";
-                break;
-            case 'command':
-                output = "__`View custom command list or set your own commands`__\n\n";        
-                output += "**Usage examples:**\n`!cl` Shows list of of available custom created commands as a DM (Due to character limit per post, Nero will send several messages) [DEPRECATED]\n";
-                output += "`!add !yourcommand http://i.imgur.com/YrgluxT.gif` Adds your own command\n";
-                output += "`!find search_value` Find a command\n";
-                output += "`!rename !old !new` Rename a command\n";
-                output += "`!edit !yourcommand http://i.imgur.com/YrgluxT.gif` Edits value of the command\n";
-                output += "`!delete !yourcommand` Deletes the command [Admin only]\n";
-                break;
-            default:
-                output = "";
-        }
-		await interaction.reply(output);
+
+		await interaction.reply(getHelpText(category));
 	},
+    run: (client, message, args) => {
+
+        if (args == "") {
+            var category = "none";
+        }
+        else {
+            category = args[0].toLowerCase();
+        }
+        let output = getHelpText(category);
+        
+        message.channel.send(output);
+    }
 };
-
-exports.run = (client, message, args) => {
-
-    if (args == "") {
-        let output = ":clipboard: | **Nero Command List**\n";
-        output += "Use `!help <command>` to get more info on a specific command, for example: `!help bday`\n\n";
-        output += "`bday`, `command`, `opt`, `fgo`"
-
-        message.channel.send(output);
-    }
-    else if (args[0].toLowerCase() == "bday") {
-        let output = "`!bday` __`View birthdays of members or set your own birthday`__\n\n";
-        output += "**Usage:** `!bday [set] <DD/MM>` | `!bday <user>`\n\n";
-        output += "**Examples:**\n`!bday` Shows list of birthdays of members\n`!bday set 12/05` Set or update your birthday\n`!bday Nero` View user's birthday (search or mention)\n\n";
-
-        message.channel.send(output);
-    }
-    else if (args[0].toLowerCase() == "command") {
-        let output = "__`View custom command list or set your own commands`__\n\n";
-        //output += "**Usage:** !command [!yourcommand | list | rename | url | delete] [<url> | <!old !new> | <!yourcommand url> | <!todeletecommand>]\n\n";
-
-        output += "**Usage examples:**\n`!cl` Shows list of of available custom created commands as a DM (Due to character limit per post, Nero will send several messages) [DEPRECATED]\n";
-        output += "`!add !yourcommand http://i.imgur.com/YrgluxT.gif` Adds your own command\n";
-        output += "`!find search_value` Find a command\n";
-        output += "`!rename !old !new` Rename a command\n";
-        output += "`!edit !yourcommand http://i.imgur.com/YrgluxT.gif` Edits value of the command\n";
-        output += "`!delete !yourcommand` Deletes the command [Admin only]\n";
-
-        message.channel.send(output);
-    }
-    else if (args[0].toLowerCase() == "opt") {
-        let output = "`!optin | !optout` __`Adds or removes mentionable role`__\n\n";
-
-        output += "**Usage:** `!optin <role>` | `!optout <role>`\n";
-        output += "**Args:** `gw`\n\n";
-        output += "**Examples:**\n`!optin gw` Opts you in the Group Watch role\n";
-        output += "`!optout gw` Opts you out of the Group Watch role";
-
-        message.channel.send(output);
-    }
-    else if (args[0].toLowerCase() == "fgo") {
-        let output = "`!fgo-show | !fgo-edit` __`View or update FGO profile`__\n\n";
-
-        output += "**Usage:** `!fgo-show <user>` | `!fgo-edit [IGN: ign | ID: id | Region: <JP|NA|TW|CH|KR>]` [Add screenshot as attachment!]\n\n";
-        output += "**Examples:**\n`!fgo-show` Shows own FGO profile\n";
-        output += "`!fgo-show Nero` Shows user's FGO profile (search or mention)\n";
-        output += "`!fgo-edit IGN: Duwangel | ID: 123,456,789 | Region: JP` Creates or updates your FGO profile\n";
-        output += "`!fgo-edit IGN: Yosoku Assistant ` Updates only your IGN";
-
-        message.channel.send(output);
-    }
-}
